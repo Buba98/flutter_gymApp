fd5fa82747ab78cf3884d1f23fbf65fe57be08b4
Vincenzo Greco <grecovincenzo98@gmail.com>
2020-08-15 11:06:47 +0200

    training schedule details page created

--- flutter_gym_app/lib/screen/training_schedule_details_page.dart
+++ flutter_gym_app/lib/screen/training_schedule_details_page.dart
@@ -2,9 +2,28 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_gym_app/utils/training/training_schedule.dart';
 
 class TrainingScheduleDetailsPage extends StatefulWidget{
-  BuildContext context;
-  TrainingSchedule trainingSchedule;
+  final BuildContext context;
+  final TrainingSchedule trainingSchedule;
 
-  TrainingScheduleDetailsPage({BuildContext context, TrainingSchedule trainingSchedule})
+  TrainingScheduleDetailsPage({this.context, this.trainingSchedule})
 
+  @override
+  State<StatefulWidget> createState() => _TrainingScheduleDetailsPageState(context: context, trainingSchedule: trainingSchedule);
 }
+
+class _TrainingScheduleDetailsPageState extends State<TrainingScheduleDetailsPage>{
+
+  final BuildContext context;
+  final TrainingSchedule trainingSchedule;
+
+  _TrainingScheduleDetailsPageState({this.context, this.trainingSchedule});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+
+
